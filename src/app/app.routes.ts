41fb70a53f353c36dae1d099695f0ba8cf55f3b8
eRodrigoL <e.rodrigo.l@outlook.com>
2026-03-31 21:59:22 -0300

    feat: adiciona rotas para painel inicial, listagem, criação e edição de chamados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Painel Inicial
+  {
+    path: '',
+    loadComponent: () =>
+      import('@pages/painel/painel-inicial/painel-inicial').then((m) => m.PainelInicial),
+    pathMatch: 'full',
+  },
+
+  // Listagem de Chamados
+  {
+    path: 'chamados',
+    loadComponent: () =>
+      import('@pages/chamados/listagem-chamados/listagem-chamados').then((m) => m.ListagemChamados),
+  },
+
+  // Criação de Chamados
+  {
+    path: 'chamados/novo',
+    loadComponent: () =>
+      import('@pages/chamados/criacao-chamado/criacao-chamado').then((m) => m.CriacaoChamado),
+  },
+
+  // Edição de Chamados
+  {
+    path: 'chamados/:id/editar',
+    loadComponent: () =>
+      import('@pages/chamados/edicao-chamado/edicao-chamado').then((m) => m.EdicaoChamado),
+  },
+
+  // Fallback
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
