8ad343fcfc3a4b7451e07bb8698f72184b5c157e
eRodrigoL <e.rodrigo.l@outlook.com>
2026-04-01 17:03:48 -0300

    feat: adiciona navegação ao botão de criação de chamados no cabeçalho

--- src/app/shared/components/cabecalho/cabecalho.ts
+++ src/app/shared/components/cabecalho/cabecalho.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+
+import { RouterLink } from '@angular/router';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-cabecalho',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './cabecalho.html',
   styleUrl: './cabecalho.scss',
 })
