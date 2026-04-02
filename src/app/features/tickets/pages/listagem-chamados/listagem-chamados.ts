373f1ff0cbbb180824712a103fa899570c5626bc
eRodrigoL <e.rodrigo.l@outlook.com>
2026-04-02 16:16:20 -0300

    feat: implementa layout estrutural da página de listagem de chamados

--- src/app/features/tickets/pages/listagem-chamados/listagem-chamados.ts
+++ src/app/features/tickets/pages/listagem-chamados/listagem-chamados.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+import { EstadoVazio } from '@components/estado-vazio/estado-vazio';
+
 @Component({
   selector: 'app-listagem-chamados',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    EstadoVazio,
+  ],
   templateUrl: './listagem-chamados.html',
   styleUrl: './listagem-chamados.scss',
 })
