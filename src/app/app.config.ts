35307fd256689ce458fe8d13ac08bc05b75af69b
eRodrigoL <e.rodrigo.l@outlook.com>
2026-04-03 00:44:20 -0300

    feat: implementa layout estrutural do formulário de criação de chamado

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideNativeDateAdapter(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+  ],
 };
--- src/app/features/tickets/pages/criacao-chamado/criacao-chamado.ts
+++ src/app/features/tickets/pages/criacao-chamado/criacao-chamado.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
 
+import { RouterLink } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-criacao-chamado',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    RouterLink,
+  ],
   templateUrl: './criacao-chamado.html',
   styleUrl: './criacao-chamado.scss',
 })
